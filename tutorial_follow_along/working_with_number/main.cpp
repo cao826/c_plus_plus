023a20c240d964df33eaab5e3c93da67045d2402
Carlos Olivares <carlos.olivares.331@outlook.com>
2022-11-20 11:13:58 -0800

    basic math

---
+++ tutorial_follow_along/working_with_number/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int wnum = 5;
+    double dnum = 5.5;
+    cout << wnum;
+    wnum++;
+    /* Math done with two integers will return two integers */
+    cout << pow(2, 5); /*sqrt(36) round(4.3) ceil(4.1) = 5 floor(5.7) = 4 fmax(3, 10) = 10, fmin()*/
+    return 0;
+}
